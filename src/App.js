2375ffc33706264c9a20df740ac36e4fa1cd7c83
Joaquin Franich <joaquinmelo90@gmail.comm>
2022-10-21 10:42:05 -0300

    Eliminacion de archivos no utilizados.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./Routes/Navbar/Navbar";
@@ -18,24 +17,6 @@ function App() {
         </Route>
       </Routes>
     </div>
-=======
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
-import Home from './routes/Home';
-import About from './routes/About';
-import PageNotFound from './routes/PageNotFound';
-
-function App() {
-  return (
-
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </Router>
->>>>>>> a4fa71e05a012c20756889b382f478cb46015044
-  );
-}
+)}
 
 export default App;
