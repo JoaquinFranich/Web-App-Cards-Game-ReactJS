b14ea9a8ba52d817f36e60a1a69f29c900383d37
Joaquin Franich <joaquinmelo90@gmail.comm>
2022-10-24 14:19:15 -0300

    lorem ipsum

--- src/Routes/Game/Game.jsx
+++ src/Routes/Game/Game.jsx
@@ -1,17 +1,17 @@
-import { useContext, useEffect } from "react";
+import { useContext, useState } from "react";
 import Cards from "../../components/Card/Cards";
 import { CardContext } from "../../Context/CardContext";
+import {getApi} from "../../service";
 import "./Game.css";
 
 const Game = () => {
     const {cards, setCards} = useContext(CardContext);
-
+    
 
     return (
         <>
             <div className="game-container">
                 <h1>The Game</h1>
-                <Cards cards={cards}></Cards>
             </div>
         </>
 
