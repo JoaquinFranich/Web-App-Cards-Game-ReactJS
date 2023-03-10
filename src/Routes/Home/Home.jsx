ce8374f845ff6b2b59965d4ef41f9c786b0b13a1
red-mat <jesus.ing.rodriguez@gmail.com>
2023-03-09 21:00:52 -0300

    refactor: adpative changes in code

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -1,19 +1,12 @@
-import { useContext, useEffect } from "react";
-import Card from "../../components/Card/Card";
-import { CardContext } from "../../Context/CardContext";
 import "./Home.css";
 
 const Home = () => {
-
-
-
-    return(
-        <div className="main-container">
-            <h1>Home</h1>
-            <h2>Let's play a game with all this Characters!</h2>
-            <Card/>
-        </div>
-    )
-}
+  return (
+    <div className="main-container">
+      <h1>Home</h1>
+      <h2>Let's play a game with all this Characters!</h2>
+    </div>
+  );
+};
 
 export default Home;
